28295be7491f8e9a4d1321dca7f59150455fc588
ntn3397 <nguyennghia3397@gmail.com>
2018-10-03 15:19:22 +0700

    edit

--- fbbot/fbwebhook.php
+++ fbbot/fbwebhook.php
@@ -2,6 +2,11 @@
 	if (isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub_verify_token'])) {
 		if ($_GET['hub_verify_token'] =='my_verify_token')
 		echo $_GET['hub_challenge'];
+	} else {
+		$feedData = file_get_contents('php://input');
+		$handle = fopen('test.txt','w');
+		fwrite($handle, $feedData);
+		fclose($handle);
 	}
 	http_response_code(200);
 ?>
